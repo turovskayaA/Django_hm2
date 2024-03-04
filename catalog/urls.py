4b464d4639f8786e10594d6f783c835755eb72ac
anastasia turovskaya <turovskaya-nastya@mail.ru>
2024-03-04 16:55:01 +0300

    Add correct

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from catalog.views import contacts, home
+from catalog.apps import CatalogConfig
+from catalog.views import contacts, home, product_list, base_product, product_pk
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('contacts/', contacts),
-    path('', home)
+    path('contacts/', contacts, name='contacts'),
+    path('', home, name='home'),
+    path('base_product/', base_product, name='base_product'),
+    path('product_list/', product_list, name='product_list'),
+    path('<int:pk>/', product_pk, name='product_pk'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def contacts(request):
     return render(request, 'catalog/contacts.html')
@@ -7,3 +9,21 @@ def contacts(request):
 
 def home(request):
     return render(request, 'catalog/home.html')
+
+
+def base_product(request):
+    return render(request, 'catalog/base_product.html')
+
+
+def product_pk(request, pk):
+    context = {
+        'object_list': Product.objects.get(pk=pk),
+    }
+    return render(request, 'catalog/product_pk.html', context)
+
+
+def product_list(request):
+    context = {
+        'object_list': Product.objects.all(),
+    }
+    return render(request, 'catalog/product_list.html', context)
